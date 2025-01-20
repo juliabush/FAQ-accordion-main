a3f4445f31e9e72fc61de9b29d634bd28723449c
juliabush <j.elizabethbush@gmail.com>
2025-01-20 09:33:47 +0100

    Project finished

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const icons = document.querySelectorAll(".plus");
-const text = document.querySelectorAll(".answer");
+const texts = document.querySelectorAll(".answer"); 
 
-icons.forEach(icon => {
-    icon.addEventListener("click", event =>{ 
+icons.forEach((icon, index) => {
+    icon.addEventListener("click", () => { 
+        const content = texts[index]; 
+
         icon.classList.toggle("active");
 
         if (icon.classList.contains("active")) {
-            text.classList.add("visible"); // Show content
+            content.classList.add("visible"); 
         } else {
-            text.classList.remove("visible"); // Hide content
+            content.classList.remove("visible"); 
         }
     });
 });
